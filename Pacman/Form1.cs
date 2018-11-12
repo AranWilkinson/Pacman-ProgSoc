e4f8a96c56b3bf1b8e30916ef53f0b0eca4086f9
Aran Wilkinson <aran.wilkinson101@gmail.com>
2018-11-12 21:09:55 +0000

    Added walls/Keyboard controls

--- Pacman/Form1.cs
+++ Pacman/Form1.cs
@@ -20,48 +20,95 @@ namespace Pacman
         String moving;
         int Speed = 2;
         int score = 0;
+        int coinCount = 17;
         bool alive = true;
     
         private void tick_Tick(object sender, EventArgs e)
         {
             if (alive)
             {
+                move();
                 ScoreLbl.Text = "Score: " + score;
-                switch (moving)
-                {
-                    case "up":
-                        Pacman.Top -= Speed;
-                        break;
-                    case "down":
-                        Pacman.Top += Speed;
-                        break;
-                    case "left":
-                        Pacman.Left -= Speed;
-                        break;
-                    case "right":
-                        Pacman.Left += Speed;
-                        break;
-                }
                 foreach (Control c in game.Controls)
                 {
                     if (c.Bounds.IntersectsWith(Pacman.Bounds))
                     {
-                        if (c.Tag == "Coin")
+                        switch (c.Tag)
                         {
-                            if (c.Visible)
-                            {
-                                score++;
-                                c.Visible = false;
-                            }
-                        }
-                        if (c.Tag == "Ghost")
-                        {
-                            alive = false;
+                            case "Wall":
+                                switch (moving)
+                                {
+                                    case "up":
+                                        moving = "down";
+                                        break;
+                                    case "down":
+                                        moving = "up";
+                                        break;
+                                    case "left":
+                                        moving = "right";
+                                        break;
+                                    case "right":
+                                        moving = "left";
+                                        break;
+                                }
+                                move();
+                                moving = "";
+                                break;
+                            case "Coin":
+                                if (c.Visible)
+                                {
+                                    score++;
+                                    c.Visible = false;
+                                    if (score % 17 == 0 && score > 0) resetGame();
+                                }
+                                break;
+                            case "Ghost":
+                                alive = false;
+                                break;
                         }
                     }
                 }
             }
-            else Pacman.BackColor = Color.Crimson;
+            else
+            {
+                Tick.Enabled = false;
+                Pacman.BackColor = Color.Crimson;
+                MessageBox.Show("You died!");
+                score = 0;
+                resetGame();
+            }
+        }
+
+        private void move()
+        {
+            switch (moving)
+            {
+                case "up":
+                    Pacman.Top -= Speed;
+                    break;
+                case "down":
+                    Pacman.Top += Speed;
+                    break;
+                case "left":
+                    Pacman.Left -= Speed;
+                    break;
+                case "right":
+                    Pacman.Left += Speed;
+                    break;
+            }
+        }
+
+        private void resetGame()
+        {
+            Pacman.BackColor = Color.Gold;
+            alive = true;
+            moving = "";
+            Tick.Enabled = true;
+            Pacman.Location = new Point(439, 440);
+            foreach (Control c in game.Controls)
+            {
+                if (c.Tag == "Coin") c.Visible = true;
+            }
         }
 
         private void Up_Click(object sender, EventArgs e)
@@ -84,6 +131,25 @@ namespace Pacman
         private void game_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyData)
+            {
+                case Keys.Left:
+                    moving = "left";
+                    break;
+                case Keys.Right:
+                    moving = "right";
+                    break;
+                case Keys.Up:
+                    moving = "up";
+                    break;
+                case Keys.Down:
+                    moving = "down";
+                    break;
+            }
         }
     }
 }
